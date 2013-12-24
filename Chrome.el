1d7e78d4448be9a6416d020e96db6809d9caeb5b
Leo Liu <sdl.web@gmail.com>
2013-12-24 09:04:05 +0800

    Add new file Chrome.el

---
+++ Chrome.el
@@ -0,0 +1,65 @@
+;;; Chrome.el --- Chrome.app                         -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2013  Leo Liu
+
+;; Author: Leo Liu <sdl.web@gmail.com>
+;; Keywords: tools, processes
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Control Chrome from Emacs.
+
+;;; Code:
+
+(eval-when-compile
+  (require 'applescript)
+  (require 'cl))
+
+(defcustom Chrome-application-name "Google Chrome Canary"
+  "Chrome application name."
+  :type 'string
+  :group 'applescript)
+
+(defun Chrome-read-tab ()
+  (let ((tabs (split-string (read (applescript "
+tell application #{Chrome-application-name}
+  set allTabs to {}
+  repeat with w in windows
+    repeat with x in tabs of w
+      copy URL of x as text to end of allTabs
+    end repeat
+  end repeat
+  set AppleScript's text item delimiters to {\"----\"}
+  return allTabs as text
+end tell"))
+                            "----")))
+    (completing-read "Chrome tab: " tabs nil t)))
+
+;;;###autoload
+(defun Chrome-reload (&optional tab)
+  (interactive (and current-prefix-arg (list (Chrome-read-tab))))
+  (applescript "tell application #{Chrome-application-name}
+set nil to missing value
+  if #{tab} is missing value then
+    set x to active tab of front window
+  else
+    set x to first tab of some window whose URL is #{tab}
+  end if
+  tell x to reload
+end tell"))
+
+(provide 'Chrome)
+;;; Chrome.el ends here
